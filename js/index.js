f619ad9c542afb30319ee5ea2a722b1a77d3d4ca
Ching-yu-25 <ycy0227@gmail.com>
2025-02-22 21:43:52 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,62 +14,99 @@ function checkPsw(psw) {
 }
 
 $(document).ready(function () {
-
-  // 偵測方向
-  $(window).on("deviceorientation resize", function (event) {
-    if (window.matchMedia("(orientation: portrait)").matches) {
-      $('.overlay-landscape').removeClass('active');
-      $('.overlay-landscape-modal-mobile').removeClass('active');
-    }
-    if (window.matchMedia("(orientation: landscape)").matches) {
-      $('.overlay-landscape').addClass('active');
-      $('.overlay-landscape-modal-mobile').addClass('active');
-    }
-  });
-
   var scene = document.getElementById('scene');
+  console.log(document.getElementById('scene'));
   var parallaxInstance = new Parallax(scene);
-
-  $(".black").click(function () {
-    console.log(1);
-
-  })
 
   setTimeout(function () {
     $(".black").addClass("open");
   }, 3000)
 
-  $('.photos').slick({
-    dots: true,
-  });
-
-
-  $(".heart").click(heartShow);
-
-  $(".photos").dblclick(heartShow);
-
-  $(".read-more").click(function () {
-    $(".text-inner").addClass("show-more");
-    $(".read-more").hide();
-  })
-
-  $("#psw").on("keyup", function (e) {
-    if (e.which === 13) {
-      checkPsw($(this).val());
+  // 裝置方向偵測
+  $(window).on("deviceorientation resize", function (event) {
+    console.log(window.matchMedia("(orientation: portrait)"))
+    //直式
+    if (window.matchMedia("(orientation: portrait)").matches) {
+      $(".overlay-landscape-wrapper").addClass("active");
     }
-  })
+    //橫式
+    if (window.matchMedia("(orientation: landscape)").matches) {
+      $(".overlay-landscape-wrapper").removeClass("active");
 
-  $("#openbtn").on("click", function () {
-    checkPsw($("#psw").val())
+    }
   });
+
 
   $("#letter").click(function () {
-    $("#letter-wrapper").fadeIn();
-    $("#letter-content").addClass("active");
+    console.log("letter click");
+    $("#letter_wrapper").fadeIn();
+    $("#letter_content").addClass("active");
   })
 
-  $("#letter-wrapper").click(function () {
-    $("#letter-wrapper").fadeOut();
-    $("#letter-content").removeClass("active");
+  $("#letter_wrapper").click(function () {
+    $("#letter_wrapper").fadeOut();
+    $("#letter_content").removeClass("active");
   })
 })
+
+
+// $(document).ready(function () {
+
+//   // 偵測方向
+//   $(window).on("deviceorientation resize", function (event) {
+//     if (window.matchMedia("(orientation: portrait)").matches) {
+//       $('.overlay-landscape').removeClass('active');
+//       $('.overlay-landscape-modal-mobile').removeClass('active');
+//     }
+//     if (window.matchMedia("(orientation: landscape)").matches) {
+//       $('.overlay-landscape').addClass('active');
+//       $('.overlay-landscape-modal-mobile').addClass('active');
+//     }
+//   });
+
+//   var scene = document.getElementById('scene');
+//   var parallaxInstance = new Parallax(scene);
+
+//   $(".black").click(function () {
+//     console.log(1);
+
+//   })
+
+//   setTimeout(function () {
+//     $(".black").addClass("open");
+//   }, 3000)
+
+//   $('.photos').slick({
+//     dots: true,
+//   });
+
+
+//   $(".heart").click(heartShow);
+
+//   $(".photos").dblclick(heartShow);
+
+//   $(".read-more").click(function () {
+//     $(".text-inner").addClass("show-more");
+//     $(".read-more").hide();
+//   })
+
+//   $("#psw").on("keyup", function (e) {
+//     if (e.which === 13) {
+//       checkPsw($(this).val());
+//     }
+//   })
+
+//   $("#openbtn").on("click", function () {
+//     checkPsw($("#psw").val())
+//   });
+
+//   $("#letter").click(function () {
+//     $("#letter-wrapper").fadeIn();
+//     $("#letter-content").addClass("active");
+//   })
+
+//   $("#letter-wrapper").click(function () {
+//     $("#letter-wrapper").fadeOut();
+//     $("#letter-content").removeClass("active");
+//   })
+// })
